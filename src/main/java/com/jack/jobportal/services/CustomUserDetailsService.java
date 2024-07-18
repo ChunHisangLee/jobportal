4ddf4ce8e11c434bddbe671fbebe16f0d7385f9d
ChunHisangLee <goldrushkl168@gmail.com>
2024-07-18 16:40:45 +0800

    2024/07/18 - Login and Logout custom user authentication and authorization

---
+++ src/main/java/com/jack/jobportal/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.jack.jobportal.services;
+
+import com.jack.jobportal.entity.Users;
+import com.jack.jobportal.repository.UsersRepository;
+import com.jack.jobportal.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = usersRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+        return new CustomUserDetails(users);
+    }
+}
