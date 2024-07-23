4e0d97395c3eec5f461cc0c7032cf2029e57c159
ChunHisangLee <goldrushkl168@gmail.com>
2024-07-23 16:04:11 +0800

    2024/07/23 - optimize: create the AuthenticationUtils

--- src/main/java/com/jack/jobportal/controller/JobSeekerApplyController.java
+++ src/main/java/com/jack/jobportal/controller/JobSeekerApplyController.java
@@ -4,7 +4,6 @@ import com.jack.jobportal.entity.*;
 import com.jack.jobportal.services.*;
 import com.jack.jobportal.util.AuthenticationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/java/com/jack/jobportal/controller/JobSeekerSaveController.java
+++ src/main/java/com/jack/jobportal/controller/JobSeekerSaveController.java
@@ -10,7 +10,6 @@ import com.jack.jobportal.services.JobSeekerSaveService;
 import com.jack.jobportal.services.UsersService;
 import com.jack.jobportal.util.AuthenticationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
